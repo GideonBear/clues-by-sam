296351f87cc5692c0dd683a5db886068f944f2c3
Gideon <dig-sultry-sedate@duck.com>
2026-09-01 17:21:07 +0200

    c

--- src/clues_by_sam/parser.py
+++ src/clues_by_sam/parser.py
@@ -117,6 +117,16 @@ class ToClue(Transformer):  # type: ignore[type-arg]  # ruff: ignore[too-many-pu
             ),
         )
 
+    def only_region_region(self, c: tuple[Verdict, Region, Region]) -> Clue:
+        verdict, region_a, region_b = c
+        return Combined(
+            RegionClue(region_a, Count(verdict, Exact(2))),
+            RegionClue(
+                Overlap(region_a, region_b),
+                Count(verdict, Exact(1)),
+            ),
+        )
+
     def parity(self, c: tuple[Parity, Verdict, Region]) -> Clue:
         parity, verdict, region = c
         return RegionClue(region, Count(verdict, parity))
